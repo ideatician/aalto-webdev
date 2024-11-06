523b78037f3fb413d420322262b6381ca3947b87
ideatician <anssi.lehtonen1@gmail.com>
2024-11-06 13:06:59 +0200

    Countdown

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hi again!"));
+let count = 4;
 
+
+app.get("/", (c) => {
+    if (count === 1) {
+        return c.text(`Kaboom!`);
+    } else {
+        count -= 1;
+        return c.text(count);
+    }
+});
+/*
+app.get("/", 
+    (c) => c.text(`Kaboom!`)
+);
+*/
 export default app;
