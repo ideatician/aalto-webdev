1b66edd197d094d7027f0c5cac6ff5f4a8c82c04
ideatician <anssi.lehtonen1@gmail.com>
2024-11-07 10:23:14 +0200

    Trying out Deno.openKv

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
 import { getStore, setStore } from "./store.js";
 const app = new Hono();
 
-app.get("/", (c) => {
+app.get("/", async (c) => {
     const store = c.req.query("store");
     if (store) {
-        setStore(store);
+        await setStore(store);
     }
-    return c.text(`Store: ${getStore()}`);
+    return c.text(`Store: ${await getStore()}`);
 });
 
 export default app;
--- store.js
+++ store.js
@@ -1,9 +1,12 @@
-let store = 0;
+const setStore = async (s) => {
+    const kv = await Deno.openKv();
+    await kv.set(["store"], s);
+};
 
-const setStore = (s) => store = s;
-
-const getStore = () => {
-    return store;
+const getStore = async () => {
+    const kv = await Deno.openKv();
+    const s = await kv.get(["store"]);
+    return s.value ?? "Nothing.";
 };
 
 export { setStore };
